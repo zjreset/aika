f962380a41e038598fcf877ecd614eff02ea6f12
zjreset <admin.zj@163.com>
2014-04-13 08:28:05 +0800

    Initial Commit

---
+++ aika/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  aika
+//
+//  Created by runes on 14-4-13.
+//  Copyright (c) 2014年 heigesoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
